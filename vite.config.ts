43e27f5dcde3ae91c2a60685f85e78aa276dd5cc
bazzinga911 <54790688+bazzinga911@users.noreply.github.com>
2025-08-20 18:11:58 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 // vite.config.ts
 import { defineConfig } from "vite";
-import react from "vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import react from "@vitejs/plugin-react";
+import path from "node:path";
 
-export default defineConfig(({ mode }) => ({
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
-}));
+export default defineConfig({
+  plugins: [react()],
+  resolve: { alias: { "@": path.resolve(__dirname, "src") } },
+});
