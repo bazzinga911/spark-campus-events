1d3333ee083740bd0ef490f29b5bcffe47449d0c
bazzinga911 <54790688+bazzinga911@users.noreply.github.com>
2025-08-20 18:14:10 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
-// vite.config.ts
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "node:path";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: { alias: { "@": path.resolve(__dirname, "src") } },
-});
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
